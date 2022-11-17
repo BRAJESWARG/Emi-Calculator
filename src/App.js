19dbfaf820efff73a14b7817631343e8eb2459df
BRAJESWAR GHOSH <brajeswar520@gmail.com>
2022-11-17 11:07:19 +0530

    EMI-Calculator

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import './App.css';
+import CalculateEmi from "./Components/CalculateEmi";
+
+function App() {
+  return (
+    <div className="App">
+      <CalculateEmi />
+    </div>
+  );
+}
+
+export default App;
